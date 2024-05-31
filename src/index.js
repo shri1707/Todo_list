9a9e69ad3815bbeb32baa818c1e1076cdbbe6509
Shrijan Kumar <shri172004@gmail.com>
2024-05-31 19:40:17 +0530

    nothing major as such

--- src/index.js
+++ src/index.js
@@ -1,36 +1,40 @@
 import "./style.css";
-import { formValue, createTask, taskDialog } from "./task";
+import { renderTasks, formValue, createTask, taskDialog } from "./task";
 import { projectValue, createProject, projectDialog } from "./project";
 
 document.addEventListener("DOMContentLoaded", () => {
     const addTaskButton = document.querySelector(".add-task");
-    const addProjectButton= document.querySelector(".add-project")
+    const addProjectButton = document.querySelector(".add-project")
 
 
     addTaskButton.addEventListener("click", () => {
-        
+
         taskDialog()
         document.getElementById('name').focus()
     });
 
-    addProjectButton.addEventListener("click", ()=>{
+    addProjectButton.addEventListener("click", () => {
         projectDialog()
         document.getElementById('n').focus()
     })
-    
-    
+
+
     document.querySelector(".submitbtn").addEventListener("click", (event) => {
         event.preventDefault();
         formValue();
+        // After creating a task, render tasks again
+        renderTasks();
     });
 
     document.querySelector(".projectSubmitbtn").addEventListener("click", (event) => {
         event.preventDefault();
         projectValue();
     });
-    
+
     // document.querySelector(".closebtn").addEventListener("click", (event) => {
     //     event.preventDefault();
     //     document.querySelector(".taskDialog").close();
     // });
+    renderTasks();
+
 });
